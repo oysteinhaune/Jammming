7809871cccf2fbe8e832340f1d367c140af584c5
Oystein <oysteinha@gmail.com>
2021-04-01 18:46:27 +0200

    totaly new client

--- react-backend/client/src/Components/App/App.js
+++ react-backend/client/src/Components/App/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import './App.css';
-import { SearchResults} from "../SearchResults/SearchResults";
+import { SearchResults } from "../SearchResults/SearchResults";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { Playlist } from "../Playlist/Playlist";
 
 
 class App extends React.Component {
 
+    componentDidMount() {
+        fetch('/users')
+            .then(res => res.json())
+            .then(users => this.setState({ users }));
+    }
+
     constructor(props) {
         super(props)
         this.state = {
-            searchResults: [{name: "Enter Sandman", album: "BlackAlbum", artist: "Metallica", id: 1, uri: "gjkdd"},
-                {name: "Hello", album: "Kill Me", artist: "Korn", id: 2, uri: "gjkd33d"},
-                {name: "Hammer", album: "Grudge", artist: "Tool", id: 3, uri: "gjkdd44"}],
+            users: [] ,
+            searchResults: [{ name: "Enter Sandman", album: "BlackAlbum", artist: "Metallica", id: 1, uri: "gjkdd" },
+            { name: "Hello", album: "Kill Me", artist: "Korn", id: 2, uri: "gjkd33d" },
+            { name: "Hammer", album: "Grudge", artist: "Tool", id: 3, uri: "gjkdd44" }],
             playlistName: "New Playlist",
-            playlistTracks: [{name: "One", album: "BlackAlbum", artist: "Metallica", id: 4, uri: "gjkd55d"},
-                {name: "Hello", album: "Kill Me", artist: "Pantera", id: 5, uri: "gj66kdd"},
-                {name: "Hammerers", album: "Grudge", artist: "Tooler", id: 6, uri: "g32jkdd"}]
+            playlistTracks: [{ name: "One", album: "BlackAlbum", artist: "Metallica", id: 4, uri: "gjkd55d" },
+            { name: "Hello", album: "Kill Me", artist: "Pantera", id: 5, uri: "gj66kdd" },
+            { name: "Hammerers", album: "Grudge", artist: "Tooler", id: 6, uri: "g32jkdd" }]
         }
 
         this.addTrack = this.addTrack.bind(this)
@@ -32,18 +39,18 @@ class App extends React.Component {
             return
         } else {
             newPlaylist.push(track)
-            this.setState({playlistTracks: newPlaylist});
+            this.setState({ playlistTracks: newPlaylist });
         }
     }
 
     removeTrack(track) {
         let newPlaylist = this.state.playlistTracks
         newPlaylist = this.state.playlistTracks.filter(savedTrack => savedTrack.id !== track.id)
-        this.setState({playlistTracks: newPlaylist});
+        this.setState({ playlistTracks: newPlaylist });
     }
 
     updatePlaylistName(name) {
-        this.setState({playlistName: name})
+        this.setState({ playlistName: name })
     }
 
     savePlaylist() {
@@ -57,19 +64,25 @@ class App extends React.Component {
 
     render() {
         return (
-          <div>
-            <h1>Ja<span className="highlight">mmm</span>ing</h1>
-            <div className="App">
-                <SearchBar onSearch={this.search} />
-              <div className="App-playlist">
-                <SearchResults searchResult={this.state.searchResults} onAdd={this.addTrack} isPlus={true}/>
-                <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}
-                          onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist}/>
-              </div>
+            <div>
+                <h1>Ja<span className="highlight">mmm</span>ing</h1>
+                <div className="App">
+                    <h1>Users</h1>
+                    {this.state.users.map(user =>
+                        <div key={user.id}>{user.username}</div>
+                    )}
+                </div>
+                <div className="App">
+                    <SearchBar onSearch={this.search} />
+                    <div className="App-playlist">
+                        <SearchResults searchResult={this.state.searchResults} onAdd={this.addTrack} isPlus={true} />
+                        <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}
+                            onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
+                    </div>
+                </div>
             </div>
-          </div>
-            )
-        }
+        )
     }
+}
 
 export { App }
